049114dc8abbad670e238693c536a864276f1b2a
Nyunja <poptiff@gmail.com>
2024-08-24 13:41:30 +0300

    refactor: ArtistDetail to optimize data fetching feat: create BandResponse struct to encapsulate individual band data refactor: ArtistDetail to utilize BandResponse struct for cleaner data handling. refactor: Improve performance by fetching artist data directly by ID instead of looping through slices and comparing indices.

--- functions/handler.go
+++ functions/handler.go
@@ -13,6 +13,11 @@ type Response struct {
 	Data      Data
 }
 
+type BandResponse struct {
+	pageTitle string
+	Band      BandDetails
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		ServeError(w, "Page not found", http.StatusNotFound)
@@ -137,52 +142,53 @@ func ArtistDetail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Find the artist by ID
-	var artist Artist
+	// var artist Artist
 	loadData()
-	fmt.Println("Artists to search: ", id)
-	fmt.Println("All artists: ", artists)
-	for _, a := range artists {
-		if a.ID == id {
-			fmt.Println(artist)
-			artist = a
-			break
-		}
-	}
-	fmt.Println("The ID: ", artist.ID)
-	fmt.Println("The artist: ", artist)
-	if artist.ID == 0 {
+	// fmt.Println("Artists to search: ", id)
+	// fmt.Println("All artists: ", artists)
+	index := id
+	// for _, a := range artists {
+	// 	if a.ID == id {
+	// 		fmt.Println(artist)
+	// 		artist = a
+	// 		break
+	// 	}
+	// }
+	// fmt.Println("The ID: ", artist.ID)
+	// fmt.Println("The artist: ", artist)
+	if id == 0 {
 		ServeError(w, "Artist not found", http.StatusNotFound)
 		return
 	}
 
-	var location []Location
-	var artistDates []Date
-	var artistConcertDates []ConcertDate
-	var artistRelations []Relation
+	// var location []Location
+	// var artistDates []Date
+	// var artistConcertDates []ConcertDate
+	// var artistRelations []Relation
 
-	for _, loc := range locations.Index {
-		if loc.ID == id {
-			location = append(location, loc)
-		}
-	}
+	// for _, loc := range locations.Index {
+	// 	if loc.ID == id {
+	// 		location = append(location, loc)
+	// 	}
+	// }
 
-	for _, date := range dates.Index {
-		if date.ID == id {
-			artistDates = append(artistDates, date)
-		}
-	}
+	// for _, date := range dates.Index {
+	// 	if date.ID == id {
+	// 		artistDates = append(artistDates, date)
+	// 	}
+	// }
 
-	for _, concert := range concertDates.Index {
-		if concert.ID == id {
-			artistConcertDates = append(artistConcertDates, concert)
-		}
-	}
+	// for _, concert := range concertDates.Index {
+	// 	if concert.ID == id {
+	// 		artistConcertDates = append(artistConcertDates, concert)
+	// 	}
+	// }
 
-	for _, relation := range relations.Index {
-		if relation.ID == id {
-			artistRelations = append(artistRelations, relation)
-		}
-	}
+	// for _, relation := range relations.Index {
+	// 	if relation.ID == id {
+	// 		artistRelations = append(artistRelations, relation)
+	// 	}
+	// }
 
 	// Load the artist detail template
 	tmpl, err := template.ParseFiles("templates/band.html")
@@ -192,14 +198,13 @@ func ArtistDetail(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Prepare the response data
-	data := Response{
-		pageTitle: artist.Name,
-		Data: Data{
-			Artists:      []Artist{artist},
-			Locations:    Locations{Index: location},
-			Dates:        Dates{Index: artistDates},
-			ConcertDates: ConcertDates{Index: artistConcertDates},
-			Relations:    Relations{Index: artistRelations},
+	data := BandResponse{
+		pageTitle: artists[index].Name,
+		Band: BandDetails{
+			Artist:   artists[index],
+			Location: locations.Index[index],
+			Dates:    dates.Index[index],
+			Relation: relations.Index[index],
 		},
 	}
 
